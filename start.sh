e247d14bbaac4cc5a006550cd29bfd15525e0131
test00123 <109013696+test00123@users.noreply.github.com>
2022-12-04 14:11:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/test00123/testinonly.git /testinonly
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /testinonly
 fi
-cd /Eva
+cd /testinonly
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
